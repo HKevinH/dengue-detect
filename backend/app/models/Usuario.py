073c6106b015ce9467e2f692d971fc5406ee49e9
NotGabelo <gabriel345098@gmail.com>
2024-10-18 00:38:35 -0500

    Update Usuario.py

--- backend/app/models/Usuario.py
+++ backend/app/models/Usuario.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Usuario(Base):
-    __tablename__ = "items"
+    __tablename__ = "Usuario"
 
     id_usuario = Column(Integer, primary_key=True, index=True)
     nombre = Column(String(255), nullable=False)
