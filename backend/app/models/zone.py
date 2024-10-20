5e1cbcdf10c9751afc7e00ec04e0e843e16e7b33
NotGabelo <gabriel345098@gmail.com>
2024-10-20 18:33:47 -0500

    Update zone.py

--- backend/app/models/zone.py
+++ backend/app/models/zone.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from app.db.session import Base
 from sqlalchemy.orm import relationship
 class User(Base):
-    __tablename__ = "usuario"
+    __tablename__ = "zona"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name_zone = Column(String(255), nullable=False)
