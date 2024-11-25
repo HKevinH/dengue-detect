99f707aa22e2819e7f477d82617a7203a0a6a140
kesticar92 <160354366+kesticar92@users.noreply.github.com>
2024-11-25 10:49:04 -0500

    Update patien.py

--- backend/app/models/patien.py
+++ backend/app/models/patien.py
@@ -1,5 +1,29 @@
-# app/schemas/patient.py
+"""
+# PatientData Schema
 
+Este archivo define el esquema de datos `PatientData` utilizando Pydantic. Es un modelo que representa la estructura de los datos relacionados con pacientes, incluyendo síntomas y datos categóricos.
+
+## Atributos principales:
+### Síntomas (Sintomáticos):
+- Representados por columnas que contienen información numérica (por ejemplo, `fiebre`, `cefalea`).
+- Cada síntoma se modela como un atributo de tipo `int`, lo que permite registrar la intensidad o presencia de cada uno.
+
+### Datos categóricos:
+- Representan información descriptiva del paciente, como lugar de residencia, género, tipo de seguridad social, entre otros.
+- Cada atributo se modela como un `str`.
+
+## Propósito:
+Este esquema es utilizado para validar datos de entrada y facilitar la comunicación entre los endpoints de la API y la lógica del negocio. Su diseño asegura que los datos cumplan con las expectativas definidas antes de ser procesados.
+
+## Características:
+- **`orm_mode`:** Habilita la compatibilidad con objetos ORM, lo que permite que el esquema funcione con los modelos SQLAlchemy.
+- **Facilidad de validación:** Pydantic se encarga de validar los tipos y valores de los atributos automáticamente.
+
+## Cómo extender:
+- Agregar nuevos atributos si se requiere más información sobre el paciente.
+- Ajustar los tipos de datos si las necesidades del negocio cambian.
+
+"""
 from pydantic import BaseModel
 
 # Definir columnas
@@ -17,7 +41,6 @@ categorical_columns = [
 
 class PatientData(BaseModel):
     # Síntomas
-    
     fiebre: int
     cefalea: int
     dolor_retro_ocular: int
@@ -52,3 +75,6 @@ class PatientData(BaseModel):
     
     class Config:
         orm_mode = True
+        """
+        Habilita la compatibilidad con objetos ORM, permitiendo que los datos se conviertan automáticamente desde y hacia objetos del ORM (como los modelos SQLAlchemy).
+        """
